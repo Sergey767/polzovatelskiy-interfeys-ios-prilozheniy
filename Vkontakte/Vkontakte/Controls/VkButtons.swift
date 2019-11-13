802b752dd9be2b1817a7ef8ae807005d3171a5fa
Git-sergo <serezha_goryachev@list.ru>
2019-11-13 20:45:31 +0400

    выполнил шестое и седьмое практическое задание

--- Vkontakte/Vkontakte/Controls/VkButtons.swift
+++ Vkontakte/Vkontakte/Controls/VkButtons.swift
@@ -2,22 +2,13 @@
 //  VkButtons.swift
 //  Vkontakte
 //
-//  Created by Серёжа on 19/09/2019.
+//  Created by Сергей on 13.11.2019.
 //  Copyright © 2019 appleS. All rights reserved.
 //
 
 import UIKit
 
 class VkButtons: UIControl {
-    
-    @IBOutlet weak var likeButton: UIButton! {
-        didSet {
-            let unlikedImage = UIImage(named: "unliked")
-            self.likeButton.setImage(unlikedImage, for: .normal)
-            let likeImage = UIImage(named: "like")
-            self.likeButton.setImage(likeImage, for: .selected)
-        }
-    }
     
     @IBOutlet weak var commentsButton: UIButton! {
         didSet {
@@ -41,28 +32,4 @@ class VkButtons: UIControl {
     }
     
     @IBOutlet weak var viewsLabel: UILabel!
-    
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    override func layoutSubviews () {
-        let tapGR = UITapGestureRecognizer(target: self, action: #selector(VkButtons.handleTap(_:)))
-        
-        self.likeButton.addGestureRecognizer(tapGR)
-    }
-    
-    @objc func handleTap(_: UITapGestureRecognizer) {
-        
-        self.likeButton.isSelected = !self.likeButton.isSelected
-        
-        UILabel.transition(with: likeButton, duration: 0.3, options: .transitionFlipFromBottom, animations: {
-            self.likeButton
-        }, completion: nil)
-    }
 }
